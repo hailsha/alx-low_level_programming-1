1bbd1922d9d9fdf14f90ffd27479071ea5bbbb17
AturiheihiBlendon <2020blendon@gmail.com>
2022-04-08 12:30:37 -0700

    Without e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,9 @@ int main(void)
 	for(lower = 'a'; lower <= 'z'; lower++)
 	{
 		if (lower == 'e' || lower == 'q')
-		{
-			continue;
-		}
-		putchar(lower);
-		putchar('\n');
+		continue;
+		putchar(lower);		
 	}
+	putchar('\n');
 	return (0);
 }
