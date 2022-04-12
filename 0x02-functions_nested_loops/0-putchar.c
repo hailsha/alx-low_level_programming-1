7e9446957fbb42c2339353eb2fc9940b8170f8ff
AturiheihiBlendon <2020blendon@gmail.com>
2022-04-12 06:16:52 -0700

    0_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char *ch = "_putchar\n";
+	
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
 	return (0);
 }
